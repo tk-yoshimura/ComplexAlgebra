e20989d70a560112655b055c51e18c5637ed2c9d
tk-yoshimura <yoshimura8192@gmail.com>
2024-09-05 23:54:49 +0900

    add: cholesky

---
+++ ComplexAlgebra/ComplexMatrix/ComplexMatrix_Cholesky.cs
@@ -0,0 +1,51 @@
+﻿using DoubleDoubleComplex;
+
+namespace ComplexAlgebra {
+    public partial class ComplexMatrix {
+        public static ComplexMatrix Cholesky(ComplexMatrix m, bool enable_check_hermitian = true) {
+            if (!IsSquare(m)) {
+                throw new ArgumentException("not square matrix", nameof(m));
+            }
+
+            int n = m.Size;
+            
+            if ((enable_check_hermitian && !IsHermitian(m)) || !IsFinite(m)) {
+                return Invalid(n);
+            }
+
+            if (IsZero(m)) {
+                return Zero(n);
+            }
+
+            int exponent = (m.MaxExponent / 2) * 2;
+
+            ComplexMatrix u = ScaleB(m, -exponent);
+
+            Complex[,] v = new Complex[n, n];
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    Complex v_ij = u[i, j];
+                    for (int k = 0; k < j; k++) {
+                        v_ij -= v[i, k] * Complex.Conjugate(v[j, k]);
+                    }
+                    v[i, j] = v_ij / v[j, j]; 
+                }
+
+                Complex v_ii = u[i, i];
+                for (int k = 0; k < i; k++) {
+                    v_ii -= v[i, k] * Complex.Conjugate(v[i, k]);
+                }
+                v[i, i] = Complex.Sqrt(v_ii);
+
+                for (int j = i + 1; j < n; j++) {
+                    v[i, j] = Complex.Zero;
+                }
+            }
+
+            ComplexMatrix l = ScaleB(new ComplexMatrix(v, cloning: false), exponent / 2);
+
+            return l;
+        }
+    }
+}
---
+++ ComplexAlgebraTests/Matrix/MatrixTests_Cholesky.cs
@@ -0,0 +1,22 @@
+﻿using ComplexAlgebra;
+
+namespace ComplexAlgebraTests {
+    public partial class ComplexMatrixTests {
+        [TestMethod()]
+        public void CholeskyTest() {
+            for (int n = 1; n <= 16; n++) {
+                for (int i = 0; i < 16; i++) {
+                    ComplexMatrix m = TestCase.RandomMatrix(n, n);
+                    ComplexMatrix matrix = m * m.H;
+
+                    Console.WriteLine($"test: {matrix}");
+
+                    ComplexMatrix l = ComplexMatrix.Cholesky(matrix);
+                    ComplexMatrix v = l * l.H;
+
+                    Assert.IsTrue((matrix - v).Norm < 1e-25);
+                }
+            }
+        }
+    }
+}
